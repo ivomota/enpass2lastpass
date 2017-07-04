2152b3ecabc3b0194b193383ec6da2bad74b937d
Ivo Mota <ivo.mota@blip.pt>
2017-07-04 21:52:36 +0100

    small changes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-#!/bin/env node
 'use strict';
 
 const fs = require('fs');
 const _ = require('highland');
-const toCSV = require('../lib/convert');
-const parseArgs = require('../lib/parse-args');
+const toCSV = require('./lib/convert');
+const parseArgs = require('./lib/parse-args');
 
 const fatal = e => {
   process.stderr.write(e.message + '\n');
@@ -17,7 +16,12 @@ if (!_args._[0]) {
   fatal(new Error('You must specify an Enpass export file to process.'));
 }
 
+
+console.log(_args._[0]);
 _(fs.createReadStream(_args._[0], 'utf8'))
+.errors(function (err) {
+    console.log('Caught error:', err.message);      
+})
 .through(toCSV(_args))
 .on('error', fatal)
 .pipe(_args.output ? fs.createWriteStream(_args.output) : process.stdout);
